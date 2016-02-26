2b0cede61ec75715c0b1cf9e945741bf238cc315
Max Beekmans <max.beekmans@hotmail.com>
2016-02-26 15:23:02 +0100

    Created Basic concept of Wish Overview Now added the new files

---
+++ Controller/WishController.class.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Max
+ * Date: 25-Feb-16
+ * Time: 15:08
+ */
+class WishController
+{
+    function run()
+    {
+        //hier
+        $result = Database::query("select * from wish");
+        $var = "success";
+        if($result === false || $result == null){
+            $var =  "fuck jou";
+        }
+        $wishes = array();
+        $wishes[0] = new Wish("Max" , "Dave Grohl Ontmoeten" , "Verenigde Staten" , "Los Angeles");
+        $wishes[1] = new Wish("Marius" , "Werkende code schrijven" , "Nederland", "Den Bosch");
+        $wishes[2] = new Wish("Mevlüt" , "Iets opschrijven" , "Turkije", "Heusden");
+
+        render("wishOverview.php", ["title" => "Wensen overzicht", "wishes" => $wishes]);
+    }
+}
