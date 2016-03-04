a0c7f64d11dec0558356ff1fb04e96021235889f
Max Beekmans <mbeekman1@avans.nl>
2016-03-04 14:48:08 +0100

    more changes that got lost yesterday

--- View/wishOverview.php
+++ View/wishOverview.php
@@ -4,17 +4,78 @@
 <!-- * Date: 25-Feb-16-->
 <!-- * Time: 15:12-->
 <!-- */-->
-
+<!-- TODO *************************** -->
+<!-- TODO * BALK HEFT EEN WIDTH KUT *-->
+<!-- TODO ***************************-->
 <div class="container">
     <div class="row">
-        <div class="col-lg-2">
+         <span class="hidden-xs hidden-sm  hidden-md info">
+                <a class="btn btn-primary side-button" href="/Wishes/action=open_wish">
+                    <span class="glyphicon glyphicon-plus"></span>
+                </a>
+            </span>
+
+        <div class=" col-xs-12 col-lg-2">
             <h5>Wensen overzicht</h5>
-            <hr>
-            <h7><a type="button" class="btn btn-primary btn-default side-button" href="/Wishes/action=mywishes">Mijn wensen</a></h7><br>
-            <h7><a type="button" class="btn btn-default side-button" href="/Wishes/action=incompletedWishes">Onvervulde wensen</a></h7><br>
-            <h7><a type="button" class="btn btn-default side-button" href="/Wishes/action=completedWishes">Vervulde wensen</a></h7><br>
+            <hr/>
+             <span class="hidden-lg info">
+                <a class="btn btn-primary side-button" href="/Wishes/action=open_wish">
+                    <span class="glyphicon glyphicon-plus"></span>
+                </a>
+            </span>
+
+            <table>
+                <tr>
+                    <td>
+                        <a>
+                            <button type="button" class="btn btn-primary side-button">
+                                <span class="glyphicon glyphicon-align-justify"></span> Mijn wensen
+                            </button>
+                        </a>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>
+                        <a>
+                            <button type="button" class="btn btn-default side-button">
+                                <span class="glyphicon glyphicon-ok"></span> Vervulde wensen
+                            </button>
+                        </a>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>
+                        <a>
+                            <button type="button" class="btn btn-default side-button">
+                                <span class="glyphicon glyphicon-remove"></span> Onvervulde wensen
+                            </button>
+                        </a>
+                    </td>
+                </tr>
+
+            </table>
+
+
+            <!--            <h7><a type="button" class="btn btn-primary btn-default side-button" href="/Wishes/action=mywishes">Mijn-->
+            <!--                    wensen</a></h7>-->
+            <!--            <br>-->
+            <!--            <h7><a type="button" class="btn btn-default side-button" href="/Wishes/action=incompletedWishes">Onvervulde-->
+            <!--                    wensen</a></h7>-->
+            <!--            <br>-->
+            <!--            <h7><a type="button" class="btn btn-default side-button" href="/Wishes/action=completedWishes">Vervulde-->
+            <!--                    wensen</a></h7>-->
+            <!--            <br>-->
         </div>
         <div class="col-lg-10">
+
+            {if isset($wishError)}
+            <div class="form-error" id="err">Error: {htmlspecialchars($wishError)}</div>
+            {else}
+            <div id="err"></div>
+            {/if}
+
             <table class="table">
                 <thead>
                 <tr>
