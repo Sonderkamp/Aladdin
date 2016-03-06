f59299a5a75a0f80d247913477a708f9c67c5abd
jjjkolkh <jjjkolkh@avans.nl>
2016-03-06 19:24:58 +0100

    Check toegevoegd als de user zijn talent wil toevoegen

--- View/talentOverview.php
+++ View/talentOverview.php
@@ -40,12 +40,9 @@
                     <tr>
                         <td class="col-sm-12">{$talent -> name}</td>
                         <td class="col-sm-1">
-                            <form action="/talents" method="post">
-                                <input type="hidden" name="add_id" value="{$talent->id}"/>
-                            <button type="submit" name="submit" class="btn btn-default btn-sm">
+                            <button type="button" class="btn btn-default btn-sm" data-toggle="modal" data-target="#myModal{preg_replace('/\s+/', '', $talent->name)}">
                                 <span class="glyphicon glyphicon-ok"></span>
                             </button>
-                                </form>
                         </td>
                     </tr>
                 {/foreach}
@@ -54,7 +51,7 @@
     </div>
 </div>
 
-<!-- Modal -->
+<!-- Modal Remove-->
 {foreach from=$user_talents item=talent}
 <div id="myModal{preg_replace('/\s+/', '', $talent->name)}" class="modal fade" role="dialog">
   <div class="modal-dialog">
@@ -74,7 +71,40 @@
                 <button type="button" class="btn btn-default infoLeft" data-dismiss="modal">Sluiten</button>
                 <form action="/talents" method="post">
                     <input type="hidden" name="remove_id" value="{$talent->id}"/>
-                    <button type="submit" name="submit" class="btn btn-inbox info"><span class="glyphicon glyphicon-remove"></span> Verwijderen</button>
+                    <button type="submit" name="submit" class="btn btn-inbox info">
+                        <span class="glyphicon glyphicon-remove"></span> Verwijderen
+                    </button>
+                </form>
+            </div>
+        </div>
+
+    </div>
+</div>
+{/foreach}
+
+<!-- Modal Add-->
+{foreach from=$talents item=talent}
+<div id="myModal{preg_replace('/\s+/', '', $talent->name)}" class="modal fade" role="dialog">
+    <div class="modal-dialog">
+
+        <!-- Modal content-->
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">Talent toevoegen</h4>
+            </div>
+            <div class="modal-body">
+                <p>
+                    Weet u zeker dat u het talent "{$talent->name}" wilt toevoegen?
+                </p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default infoLeft" data-dismiss="modal">Sluiten</button>
+                <form action="/talents" method="post">
+                    <input type="hidden" name="add_id" value="{$talent->id}"/>
+                    <button type="submit" name="submit" class="btn btn-add btn-sm">
+                        <span class="glyphicon glyphicon-ok"></span> Toevoegen
+                    </button>
                 </form>
             </div>
         </div>
