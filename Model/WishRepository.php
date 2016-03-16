69e8e4739a5e3bfcf89f0efea8fdace974054e2d
Max Beekmans <max.beekmans@hotmail.com>
2016-03-16 13:08:38 +0100

    hotfix
    Ik had nooit een construct mogen gebruiken heb het nu weggehaald

--- Model/WishRepository.php
+++ Model/WishRepository.php
@@ -72,22 +72,20 @@ class WishRepository
     private function getUser($email) {
         $result = Database::query_safe("SELECT * FROM user WHERE user.Email = ?", array($email));
 
-        $newUser = new User(
-            $email,
-            $result[0]["Admin"],
-            $result[0]["Name"],
-            $result[0]["Surname"],
-            null,
-            $result[0]["Address"],
-            $result[0]["Handicap"],
-            $result[0]["Postalcode"],
-            $result[0]["Country"],
-            $result[0]["City"],
-            $result[0]["Dob"],
-            $result[0]["Gender"],
-            $result[0]["DisplayName"],
-            $result[0]["Initials"]
-        );
+        $newUser = new User();
+        $newUser->email = $email;
+        $newUser->isAdmin = $result[0]["Admin"];
+        $newUser->name = $result[0]["Name"];
+        $newUser->surname = $result[0]["Surname"];
+        $newUser->address = $result[0]["Address"];
+        $newUser->handicap = $result[0]["Handicap"];
+        $newUser->postalcode = $result[0]["Postalcode"];
+        $newUser->country = $result[0]["Country"];
+        $newUser->city = $result[0]["City"];
+        $newUser->dob = $result[0]["Dob"];
+        $newUser->gender = $result[0]["Gender"];
+        $newUser->displayName = $result[0]["DisplayName"];
+        $newUser->initials = $result[0]["Initials"];
 
         return $newUser;
     }
