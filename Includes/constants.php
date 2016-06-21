5a405d061c96c6735b738a35c418bc240328e757
Marius de Vogel <mvogel@student.avans.nl>
2016-06-21 19:16:39 +0200

    emailfix

--- Includes/constants.php
+++ Includes/constants.php
@@ -1,5 +1,6 @@
 <?php
 
+// ****************   Database   *************************
 // your database's name
 define("DATABASE", "aladdin_db2");
 
@@ -13,11 +14,17 @@ define("SERVER", "databases.aii.avans.nl");
 define("USERNAME", "aladdin");
 
 
-define("DEVELOPMENT", true);
+// ****************   EMAIL   ****************************
+define("EMAILADRESS", "no-reply@mariusdevogel.nl");
+define("EMAILWACHTWOORD", "A5ZLuar7DO");
+define("SMTP_SECURITY", "ssl"); // kan ook tls zijn
+define("SMTP_HOST", "smtp.transip.email");
+define("SMTP_PORT", 465);
 
-define("EMAILADRESS", "WebshopWebs@gmail.com");
-
-define("EMAILWACHTWOORD", "webs2eindopdracht!");
-
+// *******************************************************
 define("MOLLIE_ID", 'test_vgs9fE6musPtVGrFyzW69xM3fxVJJQ');
+
+
+
+define("DEVELOPMENT", true);
 
