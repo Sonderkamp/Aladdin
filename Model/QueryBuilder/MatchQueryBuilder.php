c468f8857f7dae61e74669737d5f6ff07b75d74c
Marius de Vogel <mvogel@student.avans.nl>
2016-06-23 01:00:28 +0200

    match fix

--- Model/QueryBuilder/MatchQueryBuilder.php
+++ Model/QueryBuilder/MatchQueryBuilder.php
@@ -19,33 +19,35 @@ class MatchQueryBuilder extends QueryBuilder
      * do not provide both since this will result in an error
      * Neither provide none cause this will cause error as well
      */
-    public function getMatches($wishId = null , $username = null, $wishUser = false){
+    public function getMatches($wishId = null, $username = null, $wishUser = false)
+    {
 
         $param = array();
 
-        if(($wishId != null && $username != null) || ($wishId == null && $username == null)){
+        if (($wishId != null && $username != null) || ($wishId == null && $username == null)) {
             return null;
         }
 
         $query = "SELECT *, `matches`.IsActive as matchIsActive FROM `matches` ";
 
-        if($username != null){
+        if ($username != null) {
             $query .= "JOIN `wish` ON `matches`.wish_Id = `wish`.Id ";
-            $query .= "JOIN `wishContent` ON `wish`.Id = `wishContent`.wish_Id ";
+            //$query .= "JOIN `wishContent` ON `wish`.Id = `wishContent`.wish_Id ";
+            $query .= "JOIN `wishContent` ON `wish`.Id = `wishContent`.wish_Id AND wishContent.Date = (Select max(Date) FROM `wishContent` WHERE wish_id = `wish`.Id) ";
         }
 
-        if($wishUser){
+        if ($wishUser) {
             $query .= "JOIN `user` ON `wish`.User = `user`.Email ";
         } else {
             $query .= "JOIN `user` ON `matches`.user_Email = `user`.Email ";
         }
 
-        if($wishId != null){
+        if ($wishId != null) {
             $query .= "WHERE `matches`.wish_Id = ? ";
             $param[] = $wishId;
         }
 
-        if($username != null){
+        if ($username != null) {
             $query .= "WHERE `matches`.user_Email = ? AND `wish`.Status = 'Vervuld' ";
             $param[] = $username;
         }
@@ -55,43 +57,48 @@ class MatchQueryBuilder extends QueryBuilder
                   AND b.Id = (SELECT Id FROM blockedUsers as c WHERE c.user_Email = `matches`.user_Email
                   ORDER BY DateBlocked DESC LIMIT 1))";
 
-        return $this->executeQuery($query , $param);
+        return $this->executeQuery($query, $param);
     }
 
-    public function addMatch($wishId , $username){
+    public function addMatch($wishId, $username)
+    {
         $query = "INSERT INTO `matches` (`wish_Id`, `user_Email`, `IsActive`, `IsSelected`) VALUES (?, ?, 1, 0);";
-        $res = $this->executeQuery($query , array($wishId , $username));
+        $res = $this->executeQuery($query, array($wishId, $username));
 
-        if($res === false) {
+        if ($res === false) {
             $query = "UPDATE `matches` SET `IsActive` = 1 WHERE `wish_Id` = ? AND `user_Email` = ?";
-            $res = $this->executeQuery($query , array($wishId , $username));
+            $res = $this->executeQuery($query, array($wishId, $username));
         }
 
         return $res;
     }
 
-    public function checkForUser($username , $wishId){
+    public function checkForUser($username, $wishId)
+    {
         $query = "SELECT * FROM `matches` WHERE `matches`.user_Email = ? AND `matches`.wish_Id = ? AND `matches`.IsActive = 1";
-        return $this->executeQuery($query , array($username, $wishId));
+        return $this->executeQuery($query, array($username, $wishId));
     }
 
-    public function clearSelected($wishId){
+    public function clearSelected($wishId)
+    {
         $query = "UPDATE `matches` SET IsSelected = 0 WHERE `matches`.wish_Id = ?";
-        $this->executeQuery($query , array($wishId));
+        $this->executeQuery($query, array($wishId));
     }
 
-    public function checkOwnWish($username, $wishId){
+    public function checkOwnWish($username, $wishId)
+    {
         $query = "SELECT * FROM `wish` WHERE `wish`.User = ? AND `wish`.Id = ?";
-        return $this->executeQuery($query , array($username , $wishId));
+        return $this->executeQuery($query, array($username, $wishId));
     }
 
-    public function selectMatch($wishId , $username){
+    public function selectMatch($wishId, $username)
+    {
         $query = "UPDATE `matches` SET IsSelected = 1 WHERE wish_Id = ? AND user_Email = ?; ";
 
-        $this->executeQuery($query , array($wishId , $username));
+        $this->executeQuery($query, array($wishId, $username));
 
         $query = "UPDATE `wish` SET Status = 'Match gevonden' WHERE `wish`.Id = ?;";
 
-        $this->executeQuery($query , array($wishId));
+        $this->executeQuery($query, array($wishId));
     }
 }
